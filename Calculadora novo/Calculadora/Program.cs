168e94e166401216de737d35e3148edd2b7b8f24
mayara.fsantos21 <mayara.fsantos21@TIT0636551W11-1>
2025-02-28 16:05:17 -0300

    Foi adicionado a formula de adição

---
+++ Calculadora novo/Calculadora/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            public decimal Resultado { get; set; };
+        public decimal Valor { get; set; };
+        private operacao operacaoSelecionada { get; set;}
+
+        private enum operacao 
+            adicao 
+            subtracao 
+switch (operacaoSelecionada)
+    }
+      case operacao.adicao:
+        resultado = valor + txtResultado.Text;
+        break
+    }
+}
