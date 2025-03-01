863d3eede3d3a03583b5660dc786f0cfeab2c449
Danyelle2 <dany.alvesbrito2@gmail.com>
2025-03-01 11:21:55 -0300

    Alteração dos nomes das variaveis e inclusão do ponto e virgula após o break.

--- Calculadora novo/Calculadora/Program.cs
+++ Calculadora novo/Calculadora/Program.cs
@@ -7,33 +7,19 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            public decimal Resultado { get; set; };
-        public decimal Valor { get; set; };
-        private operacao operacaoSelecionada { get; set; }
-
-        private enum operacao
-            adicao
-            subtracao 
-        
-         switch (operacaoSelecionada)
+                    
+         
      {
-      case operacao.adicao:
+      case operacoes.Adicao:
         Resultado = valor + Convert.ToDecimal(txtResultado.Text);
         break
 
 
-     case operacao.subtracao:
+     case operacoes.Subtracao:
         Resultado = valor - Convert.ToDecimal(txtResultado.Text);
         break
 
-            case operacao.multiplicacao
-        Resultado = valor * Convert.ToDecimal(txtResultado.Text);
-        break
-
-
-
-
-
+            
        }
     txtResultado.Text = Convert.ToString(resultado);
      }
