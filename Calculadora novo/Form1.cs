6480739e11ba1ac35c7a8825c4053209d37466de
Danyelle2 <dany.alvesbrito2@gmail.com>
2025-03-01 13:03:00 -0300

    .

--- Calculadora novo/Form1.cs
+++ Calculadora novo/Form1.cs
@@ -21,7 +21,7 @@ namespace Calculadora_novo
                     {
                         resultado = valor / Convert.ToDecimal(textBox1.Text);
                     }
-break;
+
             }
     }
 }
