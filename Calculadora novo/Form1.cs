8a5d96a36abd78038eb60d5bb61ab6fdce5af34c
Danyelle2 <dany.alvesbrito2@gmail.com>
2025-03-01 11:05:12 -0300

    Inclui as fórmulas para a divisao e multiplicação.

--- Calculadora novo/Form1.cs
+++ Calculadora novo/Form1.cs
@@ -6,5 +6,22 @@ namespace Calculadora_novo
         {
             InitializeComponent();
         }
+                case
+                    
+                    operacoes.Multiplicacao:
+                    resultado = valor* Convert.ToDecimal(textBox1.Text);
+                    break;
+
+                case operacoes.Divisao:
+                    if (Convert.ToDecimal (textBox1.Text) == 0)// se e senão (se o valor for igual a zero não dividir)
+                    {
+                        textBox1.Text = "ERRO!!\n Não é possivel dividir o número 0";
+                    }
+                    else
+                    {
+                        resultado = valor / Convert.ToDecimal(textBox1.Text);
+                    }
+break;
+            }
     }
 }
